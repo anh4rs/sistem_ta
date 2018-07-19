b7a8145ec87fe9292dd909e8e6d36cf641eefedf
Heri Hakim Setiawan <hhakimsetiawan@gmail.com>
2018-07-19 17:16:15 +0700

    new commit

--- application/models/Seminar.php
+++ application/models/Seminar.php
@@ -21,5 +21,19 @@
 		function getID(){ return $this->id; }
 		function setTanggal($tanggal){ $this->tanggal = $tanggal; }
 		function getTanggal(){ return $this->tanggal; }
+		function setJudulID($judulid){ $this->judulid = $judulid; }
+		function getJudulID(){ return $this->judulid; }
+		function setMhsID($mhsid){ $this->mhsid = $mhsid; }
+		function getMhsID(){ return $this->mhsid; }
+		function setNilaiPembimbing($nilai_pembimbing){ $this->nilai_pembimbing = $nilai_pembimbing; }
+		function getNilaiPembimbing(){ return $this->nilai_pembimbing; }
+		function setNilaiPenguji1($nilai_penguji1){ $this->nilai_penguji1 = $nilai_penguji1; }
+		function getNilaiPenguji1(){ return $this->nilai_penguji1; }
+		function setNilaiPenguji2($nilai_penguji2){ $this->nilai_penguji2 = $nilai_penguji2; }
+		function getNilaiPenguji2(){ return $this->nilai_penguji2; }
+		function setStatus($status){ $this->status = $status; }
+		function getStatus(){ return $this->status; }
+		function setStatusPengajuan($status_pengajuan){ $this->status_pengajuan = $status_pengajuan; }
+		function getStatusPengajuan(){ return $this->status_pengajuan; }
 	}
 ?>
